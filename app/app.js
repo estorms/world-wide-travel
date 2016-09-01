79de36ff2cb5acec41040cf3bf16af8f471218be
Elizabeth Sanger <libsang@gmail.com>
2016-09-01 14:32:28 -0500

    touched initial files

---
+++ app/app.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('app.js connected');
+var app = angular.module("World Wide Travel"); //define the main module with angular.module, variable name is app, variable value is world wide travel
