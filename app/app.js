6ecd3ade59baed3c6ed4fec8b88be730a2b958d0
Elizabeth Sanger <libsang@gmail.com>
2016-09-01 20:59:22 -0500

    v1 finished

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 "use strict";
 console.log('app.js connected');
-var app = angular.module("World Wide Travel"); //define the main module with angular.module, variable name is app, variable value is world wide travel
+var app = angular.module("World Wide Travel", []); //define the main module with angular.module, variable name is app, variable value is world wide travel, do this so that ng-app, which signifies a module, knows what to call itself and knows that it is a module
