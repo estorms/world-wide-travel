f7a415eb43a54432e44d5795a1ee31cba8d7a631
Elizabeth Sanger <libsang@gmail.com>
2016-09-02 14:55:16 -0500

    works with guide-routing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 "use strict";
 console.log('app.js connected');
-var app = angular.module("World Wide Travel", []); //define the main module with angular.module, variable name is app, variable value is world wide travel, do this so that ng-app, which signifies a module, knows what to call itself and knows that it is a module
+var app = angular.module("World Wide Travel", ["ngRoute"]); //define the main module with angular.module, variable name is app, variable value is world wide travel, do this so that ng-app, which signifies a module, knows what to call itself and knows that it is a module
+
+
+ app.config(function($routeProvider){
+    $routeProvider.
+        when("/items/list", {
+            templateUrl: 'partials/guide-list.html',
+            controller: "bookCtrl"                    //not URL, Url
+        });
+});
